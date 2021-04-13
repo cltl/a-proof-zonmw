04d431f80e0d5fe82f62100266527eeae5ade275
jenia <jenka@protonmail.com>
2021-04-13 12:59:21 +0200

    add cell_format parameter

--- src/utils/latex.py
+++ src/utils/latex.py
@@ -6,7 +6,13 @@ from textwrap import indent
 TABLES = []
 
 
-def show_latex(df, caption, label, column_format=None):
+def show_latex(
+    df,
+    caption,
+    label,
+    column_format=None,
+    cell_format=lambda x: f'{x:,}',
+):
     """
     Generate a LaTeX table from the `df` and append it to TABLES.
 
@@ -20,6 +26,8 @@ def show_latex(df, caption, label, column_format=None):
         label for the latex table
     column_format: str, default=None
         column format to overwrite the default one in the function
+    cell_format: func
+        function to convert `df` values (by default to str)
     
     Returns
     -------
@@ -38,7 +46,7 @@ def show_latex(df, caption, label, column_format=None):
         col_formats = [alignment.get(str(i), 'l') for i in df.dtypes.values]
         idx_formats = ['l'] * df.index.nlevels
         column_format = ''.join(idx_formats + col_formats)
-    df = df.applymap(lambda x: f'{x:,}')
+    df = df.applymap(cell_format)
     tab = ' ' * 4
     tabular = indent(df.to_latex(column_format=column_format), tab).rstrip('\n')
     table = template.substitute(tabular=tabular, caption=caption, label=label)
