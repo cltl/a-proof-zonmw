48c8ed629b1d85397ede39fcc78c834413b61147
vanboefer <jenka@protonmail.com>
2021-08-04 11:52:45 +0200

    add scripts for levels regression model

---
+++ scripts/clf_levels/evaluate_model.py
@@ -0,0 +1,101 @@
+"""
+Evaluate a fine-tuned regression model on a test set.
+Save the following outputs:
+- evaluation metrics (MSE, RMSE, MAE, eval_loss):
+    saved in a `eval_results.txt` file in the model directory
+- model outputs, wrong predictions:
+    saved in the path indicated by the respective parameters
+"""
+
+
+import argparse
+import pickle
+import warnings
+import torch
+import pandas as pd
+from simpletransformers.classification import ClassificationModel
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+
+def evaluate(
+    test_pkl,
+    model_type,
+    model_name,
+    model_outputs_path,
+    wrong_predictions_path,
+):
+    """
+    Evaluate a fine-tuned regression model on a test set.
+    Save evaluation metrics in a `eval_results.txt` file in the model directory. The metrics include: MSE, RMSE, MAE and eval_loss.
+    Save model outputs and wrong predictions in pickled files at `model_outputs_path` and `wrong_predictions_path`.
+
+    Parameters
+    ----------
+    test_pkl: str
+        path to pickled df with the training data, which must contain the columns 'text' and 'labels'; the labels are numeric values on a continuous scale
+    model_type: str
+        type of the pre-trained model, e.g. bert, roberta, electra
+    model_name: str
+        path to a directory containing model file
+    model_outputs_path: str
+        path to save the pickled model outputs
+    wrong_predictions_path: str
+        path to save the pickled wrong predictions
+
+    Returns
+    -------
+    None
+    """
+
+    # load data
+    test_data = pd.read_pickle(test_pkl)
+
+    # check CUDA
+    cuda_available = torch.cuda.is_available()
+    if not cuda_available:
+        def custom_formatwarning(msg, *args, **kwargs):
+            return str(msg) + '\n'
+        warnings.formatwarning = custom_formatwarning
+        warnings.warn('CUDA device not available; running on a CPU!')
+
+    # load model
+    model = ClassificationModel(
+        model_type,
+        model_name,
+        num_labels=1,
+        use_cuda=cuda_available,
+    )
+
+    # evaluate model
+    result, model_outputs, wrong_predictions = model.eval_model(
+        test_data,
+        mae=mean_absolute_error,
+        mse=mean_squared_error,
+        rmse=lambda y_true, y_pred: mean_squared_error(y_true, y_pred, squared=False),
+    )
+
+    # save evaluation outputs
+    with open(model_outputs_path, 'wb') as f:
+        pickle.dump(model_outputs, f)
+
+    with open(wrong_predictions_path, 'wb') as f:
+        pickle.dump(wrong_predictions, f)
+
+
+if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--test_pkl', default='../../data/expr_july/clf_levels_ADM/test.pkl')
+    argparser.add_argument('--model_type', default='roberta')
+    argparser.add_argument('--model_name', default='../../models/levels_adm_notes')
+    argparser.add_argument('--model_outputs', default='"../../models/levels_adm_notes/model_outputs_test.pkl')
+    argparser.add_argument('--wrong_predictions', default='"../../models/levels_adm_notes/wrong_predictions_test.pkl')
+    args = argparser.parse_args()
+
+    evaluate(
+        args.test_pkl,
+        args.model_type,
+        args.model_name,
+        args.model_outputs,
+        args.wrong_predictions,
+    )
---
+++ scripts/clf_levels/predict.py
@@ -0,0 +1,82 @@
+"""
+Apply a fine-tuned regression model to generate predictions.
+The text is given in a pickled df and the predictions are generated per row and saved in a new 'predictions' column.
+"""
+
+
+import argparse
+import warnings
+import torch
+import pandas as pd
+from simpletransformers.classification import ClassificationModel
+from pathlib import Path
+
+
+def predict_df(
+    data_pkl,
+    model_type,
+    model_name,
+):
+    """
+    Apply a fine-tuned regression model to generate predictions.
+    The text is given in `data_pkl` and the predictions are generated per row and saved in a 'predictions' column.
+
+    Parameters
+    ----------
+    data_pkl: str
+        path to pickled df with the data, which must contain the column 'text'
+    model_type: str
+        type of the pre-trained model, e.g. bert, roberta, electra
+    model_name: str
+        path to a directory containing model file
+
+    Returns
+    -------
+    None
+    """
+
+    # load data
+    df = pd.read_pickle(data_pkl)
+
+    # check CUDA
+    cuda_available = torch.cuda.is_available()
+    if not cuda_available:
+        def custom_formatwarning(msg, *args, **kwargs):
+            return str(msg) + '\n'
+        warnings.formatwarning = custom_formatwarning
+        warnings.warn('CUDA device not available; running on a CPU!')
+
+    # load model
+    model = ClassificationModel(
+        model_type,
+        model_name,
+        num_labels=1,
+        use_cuda=cuda_available,
+    )
+
+    # predict
+    def predict(txt):
+        predictions, _ = model.predict([txt])
+        return predictions
+
+    col = f"pred_{Path(model_name).stem}"
+    df[col] = df['text'].apply(predict)
+
+    # pkl df
+    df.to_pickle(data_pkl)
+    print(f"A column with predictions was added.\nThe updated df is saved: {data_pkl}")
+
+
+if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--data_pkl', default='../../data/expr_july/clf_levels_ADM/test.pkl')
+    argparser.add_argument('--model_type', default='roberta')
+    argparser.add_argument('--model_name', default='../../models/levels_adm_notes')
+    args = argparser.parse_args()
+
+    predict_df(
+        args.data_pkl,
+        args.model_type,
+        args.model_name,
+    )
